8189521a8b86420922d2a243ba74bc0371d19d5f
ThalitaGuimaraes <102105342+ThalitaGuimaraes@users.noreply.github.com>
2022-08-22 00:24:10 -0300

    Add files via upload

---
+++ PROJETODOCESSABORES/js/funcao.js
@@ -0,0 +1,13 @@
+function msg(){
+    alert("Sr(a) " + document.faleconosco.nome.value + ", seus dados foram enviados com sucesso.");
+}
+
+function aumento(){
+    document.faleconosco.nome.value= documentfaleconosco.nome.value.alert
+    toUpperCase();
+}
+    else{
+        alert("Sr(a) verifique as suas credenciais, caso seja o primeiro acesso, clicar sobre o botão cadastrar");
+    }
+
+}
